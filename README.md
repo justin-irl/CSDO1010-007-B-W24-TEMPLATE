8abe9ca6dfb8aae7612d3db33a4460f9902502d0
justin p <justinpreisler@gmail.com>
2024-04-10 21:58:55 -0400

    repo init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSDO1010-007-B-W24-TEMPLATE
+
 Template repo for CSDO1010-007-B-W24 labs
---
+++ main.tf
@@ -0,0 +1,30 @@
+#-----main.tf-----
+#===================
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+  required_version = ">= 1.7.5"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+#Deploy Networking Resources
+#============================
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+#Deploy Compute Resources
+#============================
+module "compute" {
+  source         = "./modules/compute"
+  subnets        = module.vpc.public_subnets
+  security_group = module.vpc.public_sg
+  subnet_ips     = module.vpc.subnet_ips
+}
